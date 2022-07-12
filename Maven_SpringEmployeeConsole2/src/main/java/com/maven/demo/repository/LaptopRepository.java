e32777f8c33f6bb68405ffcfa09a2c420c332046
MiniProjects00 <55965942+MiniProjects00@users.noreply.github.com>
2022-07-12 18:11:13 +0800

    Delete Maven_SpringEmployeeConsole2 directory

--- Maven_SpringEmployeeConsole2/src/main/java/com/maven/demo/repository/LaptopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maven.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.maven.demo.model.Laptop;
-
-public interface LaptopRepository extends JpaRepository<Laptop, Integer>, CustomLaptopRepository {
-
-}
